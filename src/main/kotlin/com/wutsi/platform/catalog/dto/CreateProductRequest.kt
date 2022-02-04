ae9ffb2f48c5527a1d5d3d540624ae861d005ae5
wutsi <herve.tchepannou@wutsi.com>
2022-02-04 17:49:42 -0500

    update schemas

--- src/main/kotlin/com/wutsi/platform/catalog/dto/CreateProductRequest.kt
+++ src/main/kotlin/com/wutsi/platform/catalog/dto/CreateProductRequest.kt
@@ -1,14 +1,12 @@
 package com.wutsi.platform.catalog.dto
 
 import kotlin.Double
-import kotlin.Long
 import kotlin.String
 
 public data class CreateProductRequest(
   public val title: String = "",
   public val summary: String? = null,
   public val description: String? = null,
-  public val categoryId: Long? = null,
   public val price: Double? = null,
   public val comparablePrice: Double? = null
 )
